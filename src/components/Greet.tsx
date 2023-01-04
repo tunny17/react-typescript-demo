eef0fae8871615bc312a094f5da07ede799a7af7
tunny17 <tvnji17@gmail.com>
2023-01-04 12:53:07 +0100

    practice files

---
+++ src/components/Greet.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+type GreetProps = {
+    name: string
+    messageCount: number
+    isLoggedIn: boolean
+}
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>
+        {
+            props.isLoggedIn 
+                ? `Welcome ${props.name}, you have ${props.messageCount} unread messages` 
+                : 'Welcome guest'
+        }
+       </h2>
+    </div>
+  )
+}
+
+export default Greet
