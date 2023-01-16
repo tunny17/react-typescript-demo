7ef5a706b803cd8dc925a7fa50fa2a92eb802e14
tunny17 <tvnji17@gmail.com>
2023-01-16 21:40:12 +0100

    event handling with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
+import Status from './components/Status';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
         }}  
       />
       <Input value="" handleChange={(event) => console.log(event)} />
+
+      <Status status='error' />
     </div>
   )
 }
