628a1c04195b7099a558a36d206b848fde9e640a
tunny17 <tvnji17@gmail.com>
2023-01-17 20:08:44 +0100

    passing props as a component and destructuring

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,13 +5,13 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = (props: InputProps) => {
+const Input = ({ value, handleChange }: InputProps) => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event)
   }
   return (
     <div>
-        <input type="text"  value={props.value} onChange={handleInputChange} />
+        <input type="text"  value={value} onChange={handleChange} />
     </div>
   )
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-
-type PersonProps = {
-    name: {
-        first: string
-        last: string
-    }
-}
+import { PersonProps } from './Person.types'
 
 const Person = (props: PersonProps) => {
   return (
